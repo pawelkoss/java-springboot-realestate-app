88182ac4ef193713ef8d681a8f77e6bc9882857d
Paweł Kossakowski <kossy77@gmail.com>
2020-03-14 20:31:10 +0100

    Added repository classes

---
+++ src/main/java/com/example/realestateapp/repositories/ClientRepository.java
@@ -0,0 +1,10 @@
+package com.example.realestateapp.repositories;
+
+import com.example.realestateapp.models.AbstractRepository;
+import com.example.realestateapp.models.Client;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClientRepository  extends AbstractRepository<Client> {
+
+}
---
+++ src/main/java/com/example/realestateapp/repositories/OfferRepository.java
@@ -0,0 +1,10 @@
+package com.example.realestateapp.repositories;
+
+import com.example.realestateapp.models.AbstractRepository;
+import com.example.realestateapp.models.Offer;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class OfferRepository extends AbstractRepository<Offer> {
+
+}
